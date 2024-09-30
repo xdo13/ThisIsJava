558a4ba306c629d735f215b944c014b44ee9e19e
xdo13 <xdo13@naver.com>
2024-09-30 12:13:43 +0900

    이것이 자바다 교재 코드 연습

---
+++ src/ch02/sec01/VariableInItialzationExample.java
@@ -0,0 +1,11 @@
+package ch02.sec01;
+
+public class VariableInItialzationExample {
+    public static void main(String[] args) {
+        int value = 1;
+
+        int result = value +10;
+
+        System.out.println(result);
+    }
+}
