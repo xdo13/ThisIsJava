a6cb5d7df5dc0441aa57575aa75c4018ab11bc0e
xdo13 <xdo13@naver.com>
2024-10-15 16:46:10 +0900

    상속 및 싱글턴 패턴 예제 실습

---
+++ src/ch06/sec14/exam01/Car.java
@@ -0,0 +1,24 @@
+package ch06.sec14.exam01;
+
+public class Car {
+    private int speed =1;
+    private boolean stop;
+
+    public int getSpeed(){
+        return speed;
+    }
+    public void setSpeed(int speed){
+        if (speed < 0){
+            this.speed = 0;
+        }else{
+            this.speed = speed;
+        }
+    }
+    public boolean isStop(){
+        return stop;
+    }
+    public void setStop(boolean stop){
+        this.stop = stop;
+        if (stop==true) this.speed = 0;
+    }
+}
