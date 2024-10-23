d7a3cc66b0f04fd3689fd15f0d80124892ff1e4f
xdo13 <xdo13@naver.com>
2024-10-23 12:01:29 +0900

    멀티 스레드

---
+++ src/ch14/sec03/exam02/BeepPrintExample.java
@@ -0,0 +1,24 @@
+package ch14.sec03.exam02;
+//멀티 스레드
+import java.awt.*;
+
+public class BeepPrintExample {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Toolkit toolkit = Toolkit.getDefaultToolkit();
+                for (int i =0; i<5; i++) {
+                    toolkit.beep();
+                    try { Thread.sleep(500);} catch (Exception e) { }
+
+                }
+            }
+        });
+        thread.start();
+        for (int i =0; i<5; i++){
+            System.out.println("띵");
+            try { Thread.sleep(500); } catch (Exception e) {}
+        }
+    }
+}
