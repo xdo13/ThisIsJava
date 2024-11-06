8f1297f1c43436282d0c5dbddf824ea94d4c4197
xdo13 <xdo13@naver.com>
2024-11-06 16:53:57 +0900

    ForEach 예제

---
+++ src/test/ForEach.java
@@ -0,0 +1,11 @@
+package test;
+
+public class ForEach {
+    public static void main(String[] args) {
+        String[] counts = { "one", "two", "three"};
+
+        for (String cnt : counts){
+            System.out.println(cnt);
+        }
+    }
+}
