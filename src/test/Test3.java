dcdede1e5e04d48940210ef4660912fb6d0e3814
xdo13 <xdo13@naver.com>
2024-11-06 16:52:49 +0900

    1에서 10까지의 합 구하기

---
+++ src/test/Test3.java
@@ -0,0 +1,11 @@
+package test;
+// 1-10 합 구하기
+public class Test3 {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        for (int i=1; i<=10; i++)
+            sum  =sum+i;
+        System.out.println(sum);
+    }
+}
